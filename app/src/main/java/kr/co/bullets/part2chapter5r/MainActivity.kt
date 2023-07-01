b29b5a1d4ca618758eccb42e86504436244695b2
Bullets-jyr <jyr@bullets.co.kr>
2023-07-01 12:37:32 +0900

    04. 뉴스 리스트 UI 만들기 (1)

--- app/src/main/java/kr/co/bullets/part2chapter5r/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter5r/MainActivity.kt
@@ -3,14 +3,18 @@ package kr.co.bullets.part2chapter5r
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import kr.co.bullets.part2chapter5r.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var newsAdapter: NewsAdapter
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://news.google.com/")
         .addConverterFactory(
@@ -21,12 +25,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        newsAdapter = NewsAdapter()
+
+        binding.newsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = newsAdapter
+        }
 
         val newsService = retrofit.create(NewsService::class.java)
         newsService.mainFeed().enqueue(object : Callback<NewsRss> {
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
                 Log.e("MainActivity", "${response.body()?.channel?.items}")
+                newsAdapter.submitList(response.body()?.channel?.items.orEmpty())
             }
 
             override fun onFailure(call: Call<NewsRss>, t: Throwable) {
---
+++ app/src/main/java/kr/co/bullets/part2chapter5r/NewsAdapter.kt
@@ -0,0 +1,43 @@
+package kr.co.bullets.part2chapter5r
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.bullets.part2chapter5r.databinding.ItemNewsBinding
+
+class NewsAdapter : ListAdapter<NewsItem, NewsAdapter.ViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemNewsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    inner class ViewHolder(private val binding: ItemNewsBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: NewsItem) {
+            binding.titleTextView.text = item.title
+        }
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<NewsItem>() {
+            override fun areItemsTheSame(oldItem: NewsItem, newItem: NewsItem): Boolean {
+                return oldItem === newItem
+            }
+
+            override fun areContentsTheSame(oldItem: NewsItem, newItem: NewsItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
