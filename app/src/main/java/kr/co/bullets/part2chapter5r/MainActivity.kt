c3da7da56747ce1aee8a45bd1ffb784b229df9f3
Bullets-jyr <jyr@bullets.co.kr>
2023-07-01 11:35:59 +0900

    03. 서버 데이터 불러오기

--- app/src/main/java/kr/co/bullets/part2chapter5r/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter5r/MainActivity.kt
@@ -2,10 +2,36 @@ package kr.co.bullets.part2chapter5r
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.tickaroo.tikxml.TikXml
+import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://news.google.com/")
+        .addConverterFactory(
+            TikXmlConverterFactory.create(
+                TikXml.Builder().exceptionOnUnreadXml(false).build()
+            )
+        ).build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsService = retrofit.create(NewsService::class.java)
+        newsService.mainFeed().enqueue(object : Callback<NewsRss> {
+            override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
+                Log.e("MainActivity", "${response.body()?.channel?.items}")
+            }
+
+            override fun onFailure(call: Call<NewsRss>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
