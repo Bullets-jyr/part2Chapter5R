270b68afc46a3b28ab8cfdf8eb819c78c1383e06
Bullets-jyr <jyr@bullets.co.kr>
2023-07-02 13:47:43 +0900

    09. 완성도 높이기

--- app/src/main/java/kr/co/bullets/part2chapter5r/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter5r/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
@@ -116,6 +117,8 @@ class MainActivity : AppCompatActivity() {
 
                 val list = response.body()?.channel?.items.orEmpty().transform()
                 newsAdapter.submitList(list)
+
+                binding.notFoundView.isVisible = list.isEmpty()
 
                 list.forEachIndexed { index, newsModel ->
                     Thread {
