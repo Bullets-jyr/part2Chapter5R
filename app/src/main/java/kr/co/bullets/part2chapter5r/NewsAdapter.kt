445d49ff3014fc2335deed1fa13baa9110df39ff
Bullets-jyr <jyr@bullets.co.kr>
2023-07-02 13:14:37 +0900

    08. 상세 페이지 UI 만들기

--- app/src/main/java/kr/co/bullets/part2chapter5r/NewsAdapter.kt
+++ app/src/main/java/kr/co/bullets/part2chapter5r/NewsAdapter.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kr.co.bullets.part2chapter5r.databinding.ItemNewsBinding
 
-class NewsAdapter : ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
+class NewsAdapter(private val onClick: (String) -> Unit) : ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -27,6 +27,9 @@ class NewsAdapter : ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
     inner class ViewHolder(private val binding: ItemNewsBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: NewsModel) {
             binding.titleTextView.text = item.title
+            binding.root.setOnClickListener {
+                onClick(item.link)
+            }
             Glide.with(binding.thumbnailImageView)
                 .load(item.imageUrl)
                 .into(binding.thumbnailImageView)
